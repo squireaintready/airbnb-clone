15ef487edcc5e729faf76520646c6240ea1a6e6e
Samuel Jo <jo.sunghak@gmail.com>
2020-09-24 11:34:05 -0400

    Updates

---
+++ precache-manifest.daa43a88a2d785fca3b3679e2560396b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e5c301a07f4a4c0dda2d69c8b6b6a8c",
+    "url": "/airbnb-clone/index.html"
+  },
+  {
+    "revision": "8143dee2db7c46f95543",
+    "url": "/airbnb-clone/static/css/2.a1840750.chunk.css"
+  },
+  {
+    "revision": "7a1f5591f67dd671a1e0",
+    "url": "/airbnb-clone/static/css/main.72a85f0b.chunk.css"
+  },
+  {
+    "revision": "8143dee2db7c46f95543",
+    "url": "/airbnb-clone/static/js/2.163638fe.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/airbnb-clone/static/js/2.163638fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a1f5591f67dd671a1e0",
+    "url": "/airbnb-clone/static/js/main.c70d3c44.chunk.js"
+  },
+  {
+    "revision": "f358a9c4d5787a3a04f2",
+    "url": "/airbnb-clone/static/js/runtime-main.fc586be8.js"
+  }
+]);
