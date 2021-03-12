c7c1853b0c9d0e3d51a20cf3221957301aa8b516
Sam Jo <jo.sunghak@gmail.com>
2021-03-12 17:17:20 -0500

    Whole project -first commit

---
+++ src/Search.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react';
+import './Search.css';
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css'
+import {DateRangePicker} from 'react-date-range'
+import PeopleIcon from '@material-ui/icons/People'
+import { Button } from '@material-ui/core';
+import {useHistory} from 'react-router-dom'
+
+function Search() {
+    const history = useHistory()
+    const [startDate, setStartDate] = useState(new Date())
+    const [endDate, setEndDate] = useState(new Date())
+    
+    const selectionRange = {
+        key: 'selection',
+        startDate: startDate,
+        endDate: endDate,
+    }
+
+    function handleSelect(ranges){
+        setStartDate(ranges.selection.startDate);
+        setEndDate(ranges.selection.endDate);
+    }
+
+    return (
+        <div className='search'>
+            <DateRangePicker ranges = {[selectionRange]} onChange={handleSelect}/>
+            <h2>Number of Guest
+                <PeopleIcon/>
+            </h2>
+            <input min={0} defaultValue={2} type='number'/>
+            <Button onClick={()=> history.push('/search')}>Search AirBnb</Button>
+        </div>
+    )
+}
+
+
+export default Search
---
+++ src/SearchPage.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import './SearchPage.css'
+import { Button } from '@material-ui/core'
+import SearchResult from './SearchResult'
+
+function SearchPage() {
+    return (
+        <div className='searchPage'>
+            <div className="searchPage__info">
+                <p>62 stays · 26 august to 30 august · 2 guest</p>
+                <h1>Stays nearby</h1>
+                <Button variant='outlined'>Type of place</Button>
+                <Button variant='outlined'>Price</Button>
+                <Button variant='outlined'>Rooms and beds</Button>
+                <Button variant='outlined'>More filters</Button>
+            </div>
+            <SearchResult
+                img='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU'
+                location='Private room in the heart of the big apple'
+                title='Stay at this spacious penthouse'
+                description= '1 guest · 1 bedroom · 1 bed · 1.5 shared bathrooms · Wifi · Kitchen · Free parking · Washing Machine'
+                star={4.73}
+                price='$70 / night'
+                total='$260 total'
+            />
+            <SearchResult
+                img='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU'
+                location='Private room in the heart of the big apple'
+                title='Stay at this spacious penthouse'
+                description= '1 guest · 1 bedroom · 1 bed · 1.5 shared bathrooms · Wifi · Kitchen · Free parking · Washing Machine'
+                star={4.73}
+                price='$70 / night'
+                total='$260 total'
+            />
+            <SearchResult
+                img='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU'
+                location='Private room in the heart of the big apple'
+                title='Stay at this spacious penthouse'
+                description= '1 guest · 1 bedroom · 1 bed · 1.5 shared bathrooms · Wifi · Kitchen · Free parking · Washing Machine'
+                star={4.73}
+                price='$70 / night'
+                total='$260 total'
+            />
+            <SearchResult
+                img='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU'
+                location='Private room in the heart of the big apple'
+                title='Stay at this spacious penthouse'
+                description= '1 guest · 1 bedroom · 1 bed · 1.5 shared bathrooms · Wifi · Kitchen · Free parking · Washing Machine'
+                star={4.73}
+                price='$70 / night'
+                total='$260 total'
+            />
+        </div>
+    )
+}
+
+export default SearchPage
